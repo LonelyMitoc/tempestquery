4eb85264f11ce0751bb9e45912151d9d77b15654
Triglyceride <jamesmatsu@gmail.com>
2022-09-26 14:40:32 -0700

    add hidden class in css and add forecast variables to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // Current date and time variables
 var currentDateEl = document.getElementById('date-time');
 var currentDate;
-var today;
 
 // Render date and time in the heading
 function renderDateTime() {
@@ -35,7 +34,49 @@ var cityInput = document.getElementById('search-input');
 var submitBtn = document.getElementById('search-btn');
 var cityHistory = document.getElementById('search-history');
 var resultContentEl = document.getElementById('result-content');
-var resultTextEl = document.getElementById('resutl-text');
+var resultTextEl = document.getElementById('result-text');
+
+var today = document.getElementById('current-date');
+var currentTemp = document.getElementById('current-temp');
+var currentWind = document.getElementById('current-wind');
+var currentHumidity = document.getElementById('current-humidity');
+var currentIcon = document.getElementById('current-weather-icon');
+
+var date1 = document.getElementById('date-1');
+var temp1 = document.getElementById('temp-1');
+var wind1 = document.getElementById('wind-1');
+var humidity1 = document.getElementById('humidity-1');
+var icon1 = document.getElementById('icon-1');
+
+var date2 = document.getElementById('date-2');
+var temp2 = document.getElementById('temp-2');
+var wind2 = document.getElementById('wind-2');
+var humidity2 = document.getElementById('humidity-2');
+var icon2 = document.getElementById('icon-2');
+
+var date3 = document.getElementById('date-3');
+var temp3 = document.getElementById('temp-3');
+var wind3 = document.getElementById('wind-3');
+var humidity3 = document.getElementById('humidity-3');
+var icon3 = document.getElementById('icon-3');
+
+var date4 = document.getElementById('date-4');
+var temp4 = document.getElementById('temp-4');
+var wind4 = document.getElementById('wind-4');
+var humidity4 = document.getElementById('humidity-4');
+var icon4 = document.getElementById('icon-4');
+
+var date5 = document.getElementById('date-5');
+var temp5 = document.getElementById('temp-5');
+var wind5 = document.getElementById('wind-5');
+var humidity5 = document.getElementById('humidity-5');
+var icon5 = document.getElementById('icon-5');
+
+date1.textContent = moment().add(1, 'days').format('MM/DD/YYYY');
+date2.textContent = moment().add(2, 'days').format('MM/DD/YYYY');
+date3.textContent = moment().add(3, 'days').format('MM/DD/YYYY');
+date4.textContent = moment().add(4, 'days').format('MM/DD/YYYY');
+date5.textContent = moment().add(5, 'days').format('MM/DD/YYYY');
 
 var previousSearch = [];
 var cityName;
