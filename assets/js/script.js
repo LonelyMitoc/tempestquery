5d88b7c266825f6643ce81e90052332b08586112
LonelyMitoc <jamesmatsu@gmail.com>
2022-09-26 00:40:04 -0700

    html, api, submit, push city history

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,14 +34,67 @@ var futureInfo = {};
 var cityInput = document.getElementById('search-input');
 var submitBtn = document.getElementById('search-btn');
 var cityHistory = document.getElementById('search-history');
+var resultContentEl = document.getElementById('result-content');
+var resultTextEl = document.getElementById('resutl-text');
 
 var previousSearch = [];
-var city;
 var cityName;
 
 function pushCityHistory () {
     var newLi = document.createElement('li');
     var historyBtn = document.createElement('button');
     cityHistory.appendChild(newLi);
-    newLi.appendChild
+    newLi.appendChild(historyBtn);
+    cityName = cityInput.value;
+    previousSearch.push(cityName);
+    historyBtn.textContent = cityName;
+    localStorage.setItem("city", JSON.stringify(previousSearch));
+    localStorage.getItem("city");
 }
+
+function printResults (resultObj) {
+    console.log(resultObj);
+
+
+}
+
+function searchApi(cityName) {
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKey}';
+
+    fetch(weatherUrl).then(function(response){
+        if (!response.ok) {
+            throw response.json();
+        }
+
+        return response.json();
+    })
+    .then(function(weatherRes){
+
+        resultTextEl.textContent = weatherRes.search.cityName;
+
+        console.log(weatherRes);
+
+        if (!weatherRes.results.length) {
+            console.log('No results found!');
+            resultContentEl.innerHTML = '<h3>No results were found, please search again.</h3>';
+        } else {
+            resultContentEl.textContent = '';
+            printResults(weatherRes.results);
+        }
+    })
+}
+
+function searchFormSubmit(event) {
+    event.preventDefault();
+
+    var searchInputVal = document.getElementById('search-input').value;
+
+    if(!searchInputVal) {
+        console.error('Need city name to search');
+        return;
+    }
+
+    searchApi(searchInputVal);
+}
+
+submitBtn.addEventListener('submit', searchFormSubmit);
